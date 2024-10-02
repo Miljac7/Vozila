d70375925b1a9de7f6128b1bc2bc3e207ef2b05c
Miljac7 <152974534+Miljac7@users.noreply.github.com>
2024-10-02 09:23:49 +0200

    Add files via upload

---
+++ Vozila.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace Vozilaa
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtModel = new System.Windows.Forms.TextBox();
+            this.txtGodinaProizvodnje = new System.Windows.Forms.TextBox();
+            this.txtBrojKotaca = new System.Windows.Forms.TextBox();
+            this.btnUnesi = new System.Windows.Forms.Button();
+            this.btnObradi = new System.Windows.Forms.Button();
+            this.btnIspis = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(86, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Model";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(86, 101);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "GP vozila";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(86, 140);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(87, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "broj kotaca";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(124, 364);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(51, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "label4";
+            // 
+            // txtModel
+            // 
+            this.txtModel.Location = new System.Drawing.Point(218, 58);
+            this.txtModel.Name = "txtModel";
+            this.txtModel.Size = new System.Drawing.Size(239, 26);
+            this.txtModel.TabIndex = 4;
+            // 
+            // txtGodinaProizvodnje
+            // 
+            this.txtGodinaProizvodnje.Location = new System.Drawing.Point(218, 111);
+            this.txtGodinaProizvodnje.Name = "txtGodinaProizvodnje";
+            this.txtGodinaProizvodnje.Size = new System.Drawing.Size(239, 26);
+            this.txtGodinaProizvodnje.TabIndex = 5;
+            this.txtGodinaProizvodnje.TextChanged += new System.EventHandler(this.txtGodinaProizvodnje_TextChanged);
+            // 
+            // txtBrojKotaca
+            // 
+            this.txtBrojKotaca.Location = new System.Drawing.Point(218, 156);
+            this.txtBrojKotaca.Name = "txtBrojKotaca";
+            this.txtBrojKotaca.Size = new System.Drawing.Size(239, 26);
+            this.txtBrojKotaca.TabIndex = 6;
+            // 
+            // btnUnesi
+            // 
+            this.btnUnesi.Location = new System.Drawing.Point(41, 255);
+            this.btnUnesi.Name = "btnUnesi";
+            this.btnUnesi.Size = new System.Drawing.Size(97, 44);
+            this.btnUnesi.TabIndex = 7;
+            this.btnUnesi.Text = "Upis";
+            this.btnUnesi.UseVisualStyleBackColor = true;
+            this.btnUnesi.Click += new System.EventHandler(this.btnUnesi_Click);
+            // 
+            // btnObradi
+            // 
+            this.btnObradi.Location = new System.Drawing.Point(194, 255);
+            this.btnObradi.Name = "btnObradi";
+            this.btnObradi.Size = new System.Drawing.Size(93, 44);
+            this.btnObradi.TabIndex = 8;
+            this.btnObradi.Text = "Obrada";
+            this.btnObradi.UseVisualStyleBackColor = true;
+            // 
+            // btnIspis
+            // 
+            this.btnIspis.Location = new System.Drawing.Point(343, 255);
+            this.btnIspis.Name = "btnIspis";
+            this.btnIspis.Size = new System.Drawing.Size(89, 44);
+            this.btnIspis.TabIndex = 9;
+            this.btnIspis.Text = "Ispis";
+            this.btnIspis.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(496, 58);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(292, 139);
+            this.textBox1.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnIspis);
+            this.Controls.Add(this.btnObradi);
+            this.Controls.Add(this.btnUnesi);
+            this.Controls.Add(this.txtBrojKotaca);
+            this.Controls.Add(this.txtGodinaProizvodnje);
+            this.Controls.Add(this.txtModel);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtModel;
+        private System.Windows.Forms.TextBox txtGodinaProizvodnje;
+        private System.Windows.Forms.TextBox txtBrojKotaca;
+        private System.Windows.Forms.Button btnUnesi;
+        private System.Windows.Forms.Button btnObradi;
+        private System.Windows.Forms.Button btnIspis;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Vozila.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Vozilaa
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private int motociklCount = 0;
+        private int automobilCount = 0;
+        private int kamionCount = 0;
+        private object errorProvider1;
+
+        private void btnUnesi_Click(object sender, EventArgs e)
+        {
+
+            string model = txtModel.Text;
+            int GodinaProizvodnje = int.Parse(txtGodinaProizvodnje.Text);
+            int BrojKotaca = int.Parse(txtBrojKotaca.Text);
+
+            string kategorija = GetKategorija(BrojKotaca);
+
+           
+            switch (kategorija)
+            {
+                case "Motocikl":
+                    motociklCount++;
+                    break;
+                case "Automobil":
+                    automobilCount++;
+                    break;
+                case "Kamion":
+                    kamionCount++;
+                    break;
+            }
+
+           
+            btnIspis.Text = $"Model: {model}, Godina proizvodnje: {GodinaProizvodnje}, Broj kotača: {BrojKotaca}, Kategorija: {kategorija}" +
+                Environment.NewLine +
+                $"Motocikl: {motociklCount}, Automobil: {automobilCount}, Kamion: {kamionCount}";
+        }
+
+        private string GetKategorija(int brojKotaca)
+        {
+            if (brojKotaca == 2)
+            {
+                return "Motocikl";
+            }
+            else if (brojKotaca == 4)
+            {
+                return "Automobil";
+            }
+            else
+            {
+                return "Kamion";
+            }
+        }
+
+        private void btnObradi_Click(object sender, EventArgs e)
+        {
+            
+            txtModel.Text = "";
+            txtGodinaProizvodnje.Text = "";
+            txtBrojKotaca.Text = "";
+        }
+
+        private void btnIspis_Click(object sender, EventArgs e)
+        {
+
+            btnIspis.Text = $"Motocikl: {motociklCount}, Automobil: {automobilCount}, Kamion: {kamionCount}";
+
+        }
+
+        private void txtGodinaProizvodnje_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
